770f10b460937750e4c995f4e4a2d63a73a9c046
Fredesa <67815106+Fredesa@users.noreply.github.com>
2025-05-16 13:32:43 -0500

    Update scriptAutomatitionAzureGalatea.sh

--- scripts/scriptAutomatitionAzureGalatea.sh
+++ scripts/scriptAutomatitionAzureGalatea.sh
@@ -23,12 +23,12 @@ ls .
 
 #Update template issue
 mkdir -p $VAR_NAME_ROOT_REPOSITORY/.github/ISSUE_TEMPLATE/
-cp -n "galatea/report_issue_galatea.yaml" "$VAR_NAME_ROOT_REPOSITORY/.github/ISSUE_TEMPLATE/report_issue.yaml"
+cp ---update=none  "galatea/report_issue_galatea.yaml" "$VAR_NAME_ROOT_REPOSITORY/.github/ISSUE_TEMPLATE/report_issue.yaml"
 
 #Add scripts python
 mkdir -p $VAR_NAME_ROOT_REPOSITORY/scripts/
-cp -n "galatea/scripts/"* "$VAR_NAME_ROOT_REPOSITORY/scripts/"
+cp --update=none  "galatea/scripts/"* "$VAR_NAME_ROOT_REPOSITORY/scripts/"
 
 #Add actions
-cp -n "rgalatea/workflows/"* "$VAR_NAME_ROOT_REPOSITORY/.github/workflow/"
+cp --update=none  "galatea/workflows/"* "$VAR_NAME_ROOT_REPOSITORY/.github/workflow/"
 
